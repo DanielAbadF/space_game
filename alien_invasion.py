742a7d942b4b8c6ab97b877b92e6ef2dd0b38c44
Daniel <abadd486@gmail.com>
2024-07-13 11:12:35 -0400

    backgrund color

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,10 +6,16 @@ def run_game():
     screen = pygame.display.set_mode((800,600))
     pygame.display.set_caption("Invasion alienigena")
 
+    #Stablish background color
+    bg_color = (230,230,230)
+
+    #Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(bg_color)
         pygame.display.flip()
 
 run_game()
