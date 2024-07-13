11efcc04069d42f44d6a431f1886f2484c226327
Daniel <abadd486@gmail.com>
2024-07-13 11:33:10 -0400

    config class

---
+++ config.py
@@ -0,0 +1,7 @@
+class config:
+
+    def __init__(self) -> None:
+        """Initialize the game congurations"""
+        self.screen_width = 800
+        self.screen_heigth = 600
+        self.bg_color = (230,230,230)
